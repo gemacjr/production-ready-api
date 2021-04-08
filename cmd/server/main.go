b573e5e38fc3acabea4a7093ce6ca3ac1e779152
Ed McCormic <ed@edmccormic.com>
2021-04-08 01:19:23 -0400

    added stuff

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
 
-func main(){
-	fmt.Println("Go Rest API Course")
+	transportHTTP "github.com/gemacjr/go-rest-api-course/internal/transport/http"
+	
+)
+
+// App - the struct which contains things like
+// pointers to database conections
+type App struct{}
+
+// Run - handles the startup of our application
+func (app *App) Run() error {
+	fmt.Println("Setting Up Our APP")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	fmt.Println("Go REST API Course")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gemacjr/production-ready-api
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0 // indirect
